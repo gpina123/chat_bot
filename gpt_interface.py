daf22e1beba5e7a60e3c416b46b872eb3cde625e
gpina123 <160620320+gpina123@users.noreply.github.com>
2024-04-11 02:20:07 +0100

    Update gpt_interface.py

--- gpt_interface.py
+++ gpt_interface.py
@@ -35,27 +35,30 @@ header = html.Header(className='w3-container w3-center', style={'padding': '128p
 # Chatbot Layout
 chatbot_layout = html.Div(id='link2', className='w3-row-padding w3-light-grey w3-padding-64 w3-container', children=[
     html.Div(className='w3-content', children=[
-        html.Div(children=[ 
-               dbc.Row([
-                    dbc.Col(
-                        #dcc.Input(id='input-box', type='text', placeholder='Enter your text',
-                        #          style={"width":"100%"}),
+        html.Div([
+            html.Div(id='output_from_gpt',style={"padding-bottom":"10px"}),
+            html.Div(children=[ 
+                    html.Div(
                         dmc.Textarea(
                             id="input-box",
-                            label="User input",
                             placeholder="Enter your text",
                             style={"width": "100%", "font-size": "80px"},
                             autosize=True,
                             minRows=1,
                         ),
-                        width=9,
-                        style={"display":"inline"}
                     ),
-                    dbc.Col(
-                        html.Button('Submit', id='button'), width=3,style={"display":"inline"}
-                    ),
-                ], id='input-container'),
-            dbc.Row(id='output_from_gpt'),
+                    html.Div([
+                        html.Button('Submit', id='button'),
+                        dbc.Button(
+                                    [dbc.Spinner(size="sm"), " Loading..."],
+                                    color="primary",
+                                    disabled=True,
+                                    id="loading_button",
+                                    style={"display":"None"})
+                                
+                    ]),
+                ], id='input-container'
+            ),
         ])
     ]),
 ])
@@ -125,7 +128,9 @@ def display_page(pathname):
 ##########################################
 
 @app.callback(
-    Output('output_from_gpt', 'children'),
+    [Output('output_from_gpt', 'children'),
+     Output("loading_button","style"),
+     Output("input-box","value"),],
     [Input('button', 'n_clicks')],
     [State('input-box', 'value')],
     prevent_initial_call=True
@@ -139,29 +144,25 @@ def update_output(n_clicks, value):
         output = call_chatgpt_v2.get_gpt_output()
         function_response = output.get("function_response")
         if function_response is None:
-            display = html.Div(f'{output["text_response"]}')
+            display = html.Div(f'[Output] {output["text_response"]}')
         elif isinstance(function_response,str):
-            display = html.Div([html.Div(f'{output["text_response"]}'),
-                                html.Div(f'{function_response}')])
+            display = html.Div(f'[Output] {output["text_response"]}')
         else:
-            display = html.Div([html.Div(f'{output["text_response"]}'),
+            display = html.Div([html.Div(f'[Output] {output["text_response"]}'),
                                 dcc.Graph(figure=function_response)])
         
         return html.Div([
-            display,
-            html.Div([
-                    dmc.Textarea(
-                        id="input-box",
-                        label="User input",
-                        placeholder="Enter your text",
-                        style={"width": "100%"},
-                        autosize=True,
-                        minRows=1,
-                    ),
-                    html.Button('Submit', id='button')  # This is the correct ID
-            ]),
-            html.Div(id="output_from_gpt")
-            ])
+                html.Div(f"[Input] {value}"),
+                display,
+                html.Div(id="output_from_gpt",style={"padding-bottom":"10px"})
+                ],style={"padding-top":"10px"}),{"display":"None"},""
+        
+@app.callback(Output("loading_button","style",allow_duplicate=True),
+              Input("button", "n_clicks"),
+              prevent_initial_call=True)
+def appear_loading_button(n_clicks):
+    if n_clicks:
+        return {}       
         
 if __name__ == '__main__':
     app.run_server(debug=True)
